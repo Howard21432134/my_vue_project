950154855530aac3d108c187b0669ec4136681ac
Howard <978118325@qq.com>
2017-11-19 20:18:07 +0800

    a basic to do list

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,22 +1,47 @@
 
 <template>
   <div id="app">
-    <h1>{{title}}</h1>
+    <h1 v-text = "title"></h1>
+    <input v-model = "newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li v-for="item in items" v-bind:class = "{finished: item.isFinished}" v-on:click = "toggleFinish(item)">
+        {{item.label}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 export default {
   data : function(){
     return{
-      title:'this is a todo list'
+      title:'this is a todo list',
+      items:[
+
+      ],
+      newItem:''
+    }
+  },
+  methods:{
+    toggleFinish: function(item){
+      item.isFinished = !item.isFinished
+    },
+    addNew:function(){
+      this.items.push({
+        label:this.newItem,
+        isFinished:false
+      })
+      this.newItem=''
     }
   }
 }
 </script>
 
 <style>
+.finished{
+  text-decoration:underline;
+}
+
 html{
 height :100%;
 }
